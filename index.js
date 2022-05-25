7025d69592316eb8a0c6edf897b461aa16dbf745
Akin Akinbobola <akinbobola71@gmail.com>
2022-05-25 11:05:00 +0100

    Fixed the buttons

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const container = document.querySelector(`div[class="container"]`);
 const sliderValue = document.getElementById("myRange").value;
 let slider = document.getElementById("myRange");
 let output = document.getElementById("slider-output");
+let clearBtn = document.getElementById("clear");
 let colour = "black";
 
 
 let sliderOutput = () => {
     output.innerHTML = `${slider.value} X ${slider.value}`;
 }
-sliderOutput();
+
 
 slider.oninput = function() {
     output.innerHTML = `${this.value} X ${this.value}`;
 }
 
+function changeColour(newCol){
+    colour = newCol;
+}
 
 const createGrid = (size) =>{
     container.querySelectorAll("div").forEach(div => div.remove()); // Clear all existing divs
@@ -24,34 +28,30 @@ const createGrid = (size) =>{
         for (let j = 0; j < size; j++) {
             let new_div = document.createElement("div");
             new_div.style.border = `1px solid gray`;
+            new_div.addEventListener("mouseover", changeColour);
             new_div.classList.add("grid-item");
             container.appendChild(new_div);
         }
     }
 }
 
-createGrid(sliderValue);
 
 const setup = (value) => {
     createGrid(value);
 }
 
-const changeColour = (newColour) => {
-    colour = newColour;
-}
-const grid_items = document.querySelectorAll(`div[class="grid-item"]`);
 
-
-
-
-let clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", () => {
+function clear () {
+    let grid_items = document.querySelectorAll(`div[class="grid-item"]`);
     grid_items.forEach(item => item.style.backgroundColor = "white");
-});
+}
 
 
 
-grid_items.forEach(item => item.addEventListener("mouseover", () => {
-    item.style.backgroundColor = colour;
-}));
+
+clearBtn.addEventListener("click", clear);
+createGrid(sliderValue);
+sliderOutput();
+
+
 
