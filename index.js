950e2c698555450f32f8f5f9822cdd8d125fa32a
Akin Akinbobola <akinbobola71@gmail.com>
2022-05-25 10:11:27 +0100

    Dynamically creating grid

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 const container = document.querySelector(`div[class="container"]`);
 const sliderValue = document.getElementById("myRange").value;
-
-
-
 let slider = document.getElementById("myRange");
 let output = document.getElementById("slider-output");
-output.innerHTML = `${slider.value} X ${slider.value}`; // Display the default slider value
 
+let sliderOutput = () => {
+    output.innerHTML = `${slider.value} X ${slider.value}`;
+}
+sliderOutput();
 
 slider.oninput = function() {
     output.innerHTML = `${this.value} X ${this.value}`;
 }
 
 
-const createGrid = () =>{
-    container.setAttribute("style", `grid-template-columns: repeat(${sliderValue}, 1fr);grid-template-rows: repeat(${sliderValue}, 1fr);`);
-    for (let i = 0; i < sliderValue; i++) {
-        for (let j = 0; j < sliderValue; j++) {
+const createGrid = (size) =>{
+    container.querySelectorAll("div").forEach(div => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             let new_div = document.createElement("div");
+            new_div.style.border = `1px solid gray`;
             new_div.classList.add("grid-item");
-            new_div.setAttribute("style", "border: 1px grey solid");
             container.appendChild(new_div);
         }
     }
 }
-createGrid();
+
+createGrid(sliderValue);
+
+const setUp = (value) =>{
+    createGrid(value);
+}
+
 
 
 const grid_items = document.querySelectorAll(`div[class="grid-item"]`);
