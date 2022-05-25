6fd25f2724868806e180a7875e6de4210886432e
Akin Akinbobola <akinbobola71@gmail.com>
2022-05-25 09:19:17 +0100

    Webstrom Integration Files

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const container = document.querySelector(`div[class="container"]`);
-
-
 const sliderValue = document.getElementById("myRange").value;
-container.setAttribute("style", `grid-template-columns: repeat(${sliderValue}, 1fr);        grid-template-rows: repeat(${sliderValue}, 1fr);`);
-for(let i =0; i < sliderValue; i ++){
-    for(let j =0; j< sliderValue; j++){
-        new_div = document.createElement("div");
-        new_div.classList.add("grid-item");
-        new_div.setAttribute("style", "border: 1px grey solid");
-        container.appendChild(new_div);
-    }
-}
 
-const grid_items = document.querySelectorAll(`div[class="grid-item"]`);
 
-grid_items.forEach(item => item.addEventListener("mouseover", (e)=>{
-    console.log(item);
-    item.classList.add("new-color");
-}));
 
 let slider = document.getElementById("myRange");
 let output = document.getElementById("slider-output");
@@ -25,5 +9,27 @@ output.innerHTML = `${slider.value} X ${slider.value}`; // Display the default s
 
 
 slider.oninput = function() {
-  output.innerHTML = `${this.value} X ${this.value}`;
+    output.innerHTML = `${this.value} X ${this.value}`;
 }
+
+
+const createGrid = () =>{
+    container.setAttribute("style", `grid-template-columns: repeat(${sliderValue}, 1fr);grid-template-rows: repeat(${sliderValue}, 1fr);`);
+    for (let i = 0; i < sliderValue; i++) {
+        for (let j = 0; j < sliderValue; j++) {
+            let new_div = document.createElement("div");
+            new_div.classList.add("grid-item");
+            new_div.setAttribute("style", "border: 1px grey solid");
+            container.appendChild(new_div);
+        }
+    }
+}
+createGrid();
+
+
+const grid_items = document.querySelectorAll(`div[class="grid-item"]`);
+
+grid_items.forEach(item => item.addEventListener("mouseover", (e)=>{
+    item.classList.add("new-color");
+}));
+
